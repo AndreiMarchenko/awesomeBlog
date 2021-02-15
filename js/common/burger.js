86a3d2b6d788431a749f1e469a8f7f6d964932ce
AndreiMarchenko <andreymarchanka@gmail.com>
2021-02-15 12:18:09 +0300

    added burger menu

---
+++ js/common/burger.js
@@ -0,0 +1,23 @@
+"use strict"
+
+{
+    let burger = document.querySelector(".burger");
+    let header = document.querySelector(".header");
+    let headerList = document.querySelector(".header__list-wrapper");
+    let logoRef = document.querySelector(".logo__ref");
+    let body = document.querySelector("body");
+
+    let searchPanel = document.querySelector(".search__wrapper");
+
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("burger_active");
+        header.classList.toggle("header_active");
+        headerList.classList.toggle("header__list_active");
+        logoRef.classList.toggle("logo__ref_active");
+        body.classList.toggle("body_lock");
+
+        if (searchPanel !== undefined) {
+            searchPanel.classList.toggle("search__wrapper_active");
+        }
+    });
+}
