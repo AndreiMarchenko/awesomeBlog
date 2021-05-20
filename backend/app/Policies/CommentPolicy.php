01f7be48b15786a366d9007523ce5b89d0b91236
AndreiMarchenko <andreymarchanka@gmail.com>
2021-05-20 16:57:23 +0300

    improve CommentPolicy

--- backend/app/Policies/CommentPolicy.php
+++ backend/app/Policies/CommentPolicy.php
@@ -11,6 +11,6 @@ class CommentPolicy
     use HandlesAuthorization;
 
     public function delete(User $user, Comment $comment) {
-        return $user->id === $comment->user->id;
+        return $user->id === $comment->user_id;
     }
 }
